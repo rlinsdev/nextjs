ceeba2da133b21ebffab775bf6db9fdf4c1bfd23
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-03 19:16:22 -0300

    Img by next and head change

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,21 @@ import styles from '../styles/Home.module.css'
  
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title} >HomePage</h1>
-      <p className={styles.text}>tese!</p>
-      <p className={styles.text}>tese2</p>
-      <p>tese!</p>
-      <Link href="/ninjas">
-        <a className={styles.btn}> jhow! </a>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1 className={styles.title} >HomePage</h1>
+        <p className={styles.text}>tese!</p>
+        <p className={styles.text}>tese2</p>
+        <p>tese!</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}> jhow! </a>
 
-      </Link>
-    </div>
+        </Link>
+      </div>
+    </>
   )
 }
