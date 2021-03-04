942bc52470cdfb92ddfeeeeb6b46f441e160179f
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-04 10:00:35 -0300

    Dynamic routes in Next

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,8 +1,24 @@
 import style from '../../styles/Ninjas.module.css'
-const Ninja = () => {
+import Link from 'next/link'
+export const getStaticProps = async () => {
+  const result = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await result.json();
+  return {
+    props:{ ninjas: data }
+  }
+}
+
+const Ninja = ({ ninjas }) => {
   return ( 
     <div>
       <h1>Ninjas!</h1>
+      { ninjas.map(ninja => (        
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+          <a className={ style.single }>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </Link>
+      )) }
     </div>
    );
 }
